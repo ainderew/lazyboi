e7c4c90ca6a1535bc1526a17d4844229da2d35e0
Andrew Pinon <andrewpinon@192.168.254.107>
2024-02-01 22:43:30 +0800

    initial test

---
+++ index.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer')
+const cron = require('node-cron');
+
+async function clickElementByText() {
+  console.log('Running Clock In Script')
+  
+  try {
+    const browser = await puppeteer.launch({ headless: true })
+    
+    const page = await browser.newPage()
+    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+
+    // await page.setViewport({
+    //   width: 2400,
+    //   height: 800,
+    // })
+
+    await page.goto('https://theoriamedical.hrhub.ph/EmployeeDashboard.aspx')
+
+    await page.waitForSelector('#username')
+    await page.type('#username', 'apiñon', { delay: 200 });
+    await page.type('#password', 'Brainbeater321`', {delay: 200})
+
+    await page.click('#kc-login')
+    await page.waitForTimeout(15000)
+
+    const clockInOrOutXPath = `//*[text()='Clock In/Out']`;
+    const [clockInOrOutButton] = await page.$x(clockInOrOutXPath);
+    await clockInOrOutButton.click()
+
+    await page.waitForTimeout(2000)
+
+    const clockInXPath = `//*[text()='Clock In']`;
+    const [clockInButton] = await page.$x(clockInXPath)
+    await clockInButton.click()
+
+    await page.waitForTimeout(2000)
+
+    await page.click('.btn.our-button')
+
+    await page.waitForTimeout(10000)
+
+    await browser.close()
+    console.log('Done Clock In!')
+  }catch(err){
+    console.log('Something went wrong')
+    console.log(err)
+  }
+}
+
+cron.schedule("45 22 * * *", () =>{
+  console.log("waiting for 10:45")
+  clickElementByText()
+})
+
+
