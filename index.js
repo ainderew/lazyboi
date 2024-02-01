6b2098978e63522490d03e2924147bc49cda8c3e
Andrew Pinon <andrewpinon@192.168.254.107>
2024-02-01 23:31:54 +0800

    test6

--- index.js
+++ index.js
@@ -57,7 +57,11 @@ cron.schedule("* * * * *", () => {
   let now = new Date();
   console.log(now);
 });
-cron.schedule("16 23 * * *", () => {
+cron.schedule("37 23 * * *", () => {
   console.log("waiting for 10:45");
   clickElementByText();
+}, 
+{
+  scheduled: true,
+  timezone: "Asia/Manila"
 });
